ba6565fb59164cd5d9869598a19b06523a04f686
hephhay <faradaydanfard@gmail.com>
2022-08-02 05:50:42 +0100

    password reset

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -168,3 +168,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # During development only
+    
